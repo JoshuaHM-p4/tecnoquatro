c416b584e0ed1d5698c04731f6c947858b775ee7
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-04-24 16:48:26 +0800

    done with react-router-dom setup in client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/App.scss'
 
 function App() {
 
   return (
-    <>
-    <h1>Hello World</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>HOME</h1>} />
+        <Route path="/about" element={<h1>ABOUT</h1>} />
+      </Routes>
+    </Router>
   )
 }
 
