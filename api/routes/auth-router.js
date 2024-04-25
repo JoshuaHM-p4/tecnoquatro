e1c3c13a44c60b54509a4cc4ed988441c1ee62ff
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-04-25 16:03:04 +0800

    installed essential packages for authentication, created a util for error handler

--- api/routes/auth-router.js
+++ api/routes/auth-router.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import errorValidation from '../utils/errorHandler';
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.json({message: "Hello auth"})
+router.get('/signup', (req, res, next) => {
+    const {username, email, password, confirmPassword} = req.body;
+    try {
+        if (!username || !email || !password || !confirmPassword) {
+            return next(errorValidation(400, 'All fields are required'));
+        }
+        if (password !== confirmPassword) {
+            return next(errorValidation(400, 'Passwords do not match'));
+        }
+    } catch (error) {
+        next(error)
+    }
 })
 
 export default router;
