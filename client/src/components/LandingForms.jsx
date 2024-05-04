d52a355c47fe09bb80a5dbbfe2549c11ca0179df
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-05-04 20:56:45 +0800

    added redux & store to the client added signin functionality added userslice & user reducer

--- client/src/components/LandingForms.jsx
+++ client/src/components/LandingForms.jsx
@@ -1,7 +1,59 @@
-import React from "react";
-import '../styles/LandingForms.scss'; // Importing the SCSS file
+import {useState} from "react";
+import '../styles/LandingForms.scss';
+import { useNavigate } from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {
+    signInStart,
+    signInSuccess,
+    signInFailure
+} from '../redux/user/userSlice.js'
 
 const LandingForms = () => {
+    const [formBody, setFormBody] = useState({
+        email: '',
+        password: ''
+    })
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const {loading, error} = useSelector(state => state.user);
+
+    const handleChange = e => {
+        setFormBody({
+            ...formBody,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            if (!formBody.email || !formBody.password) {
+                alert('All fields are required');
+                return;
+            }
+            dispatch(signInStart());
+            const res = await fetch('/api/auth/signin', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formBody)
+            })
+            const data = await res.json();
+            if (data.success === false) {
+                dispatch(signInFailure(data.message));
+                alert(data.message);
+                return
+            }
+            dispatch(signInSuccess(data));
+            navigate('/feed');
+        } catch (error) {
+            dispatch(signInFailure(error.message));
+            console.log(error);
+        }
+    }
+
     return (
         <main className='landing-forms'> 
             <div className='forms-intro-text-container'> 
@@ -9,34 +61,44 @@ const LandingForms = () => {
             </div>
 
             <div className='forms-content'>
-                <div className='forms-login-container'>
-                    <p className='forms-login-head'>Email Address</p>
-                    <form className='forms-login'>
+                <form onSubmit={handleSubmit} className='forms-login-container'>
+                    <label className='forms-login-head'>Email address</label>
+                    <div className='forms-login'>
                         <div className="input-group">
                             <i className="material-icons input-icon">&#xe0be;</i>
-                            <input type='text' placeholder='Username' />
+                            <input 
+                                value={formBody.email}
+                                onChange={handleChange}
+                                type='text' 
+                                name="email" 
+                                placeholder='Email Address' 
+                            />
                         </div>
-                    </form>
+                    </div>
 
-                    <p className='forms-login-head'>Password</p>
-                    <form className='forms-login'>
+                    <label className='forms-login-head'>Password</label>
+                    <div className='forms-login'>
                         <div className="input-group">
                             <i className="material-icons input-icon">&#xe897;</i>
-                            <input type='password' placeholder='Password' />
+                            <input 
+                                value={formBody.password}
+                                onChange={handleChange}
+                                type='password' 
+                                name="password" 
+                                placeholder='Password' 
+                            />
                         </div>
-                    </form>
+                    </div>
 
-                    {/* Display privacy policy with radio button */}
                     <div className='forms-privacy-policy'>
                         <input type='checkbox' id='privacy-policy' name='privacy-policy' value='privacy-policy' />
                         <label htmlFor='privacy-policy'>I have read and understood the <a href="">privacy policy</a></label>
                     </div>
 
-                    {/* Display sign in button*/}
                     <div className='forms-signin-container'>
-                        <button>Sign In</button>
+                        <button disabled={loading} type="submit">Sign In</button>
                     </div>
-                </div>
+                </form>
 
                 {/* Display forms description */}
                 <div className='forms-description-container'>
